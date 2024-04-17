a7744bb6a7c330a87952e0928ff752098732251a
hiloheg <hilohe.gigi1@gmail.com>
2024-04-17 19:37:09 +0300

    Springboot init

---
+++ src/main/java/cv/project/gh/TripPlannerBE/TripPlannerBeApplication.java
@@ -0,0 +1,13 @@
+package cv.project.gh.TripPlannerBE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TripPlannerBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TripPlannerBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cv/project/gh/TripPlannerBE/TripPlannerBeApplicationTests.java
@@ -0,0 +1,13 @@
+package cv.project.gh.TripPlannerBE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TripPlannerBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
